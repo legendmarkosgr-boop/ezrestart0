d7a010b82fd37f4ab43f3ad2d8f2cdca1dff4801
legendmarkosgr-boop <legendmarkosgr@gmail.com>
2025-09-04 20:53:26 +0300

    Update settings.grandle.kts

--- EzRestart/settings.grandle.kts
+++ EzRestart/settings.grandle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "EzRestart"
+
+dependencyResolutionManagement {
+		repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+		repositories {
+			mavenCentral()
+			maven { url = uri("https://jitpack.io") }
+		}
+	}
